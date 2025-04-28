b48d6321b2c21e6eff6ed9a747f8723e6f6c7273
TabarakoAkula <mishabor7@mail.ru>
2025-04-28 23:48:08 +0300

    Add favicon for website

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url="/static/favicon.ico", permanent=True)
 
 urlpatterns = [
+    re_path(r"^favicon\.ico$", favicon_view),
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("apps.users.urls")),
     path("api/v1/questions/", include("apps.questions.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     from debug_toolbar.toolbar import debug_toolbar_urls
